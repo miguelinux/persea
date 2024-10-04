3fe0eefd00c9144c7294a9633d512a43c75e7de8
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-03 19:28:59 -0600

    run pre-commit run -a

--- src/persea/cli/__init__.py
+++ src/persea/cli/__init__.py
@@ -6,7 +6,10 @@ import click
 from persea.__about__ import __version__
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
 @click.version_option(version=__version__, prog_name="persea")
 def persea():
     click.echo("Hello world!")
