6fa99fc6a8f9af56b3a1a9bea2448318a5e677b8
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-03 20:33:25 -0600

    add modelines

--- src/persea/__init__.py
+++ src/persea/__init__.py
@@ -1,3 +1,7 @@
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
 # SPDX-FileCopyrightText: 2024-present Intel Corporation
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
