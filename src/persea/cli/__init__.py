4cbf04ce7ad491218f9528bd6802c98cfde81355
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2025-03-18 01:22:57 -0600

    persea: add option parameters

--- src/persea/cli/__init__.py
+++ src/persea/cli/__init__.py
@@ -19,17 +19,26 @@ from persea.palta import run_test
     invoke_without_command=True,
 )
 @click.version_option(version=__version__, prog_name="persea")
+@click.option("--host", help="Host from SSH config file")
+@click.option("--ssh-config", default="~/.ssh/config", help="SSH config file")
+@click.option("--test-suite-file", default="~/.config/persea/test-suite.txt", help="Test Suite file")
 @click.pass_context
-def persea(ctx):
+def persea(ctx, host, ssh_config, test_suite_file):
     """Execute test on a local or remote platform"""
+
+
+    click.echo("ssh = " + ssh_config)
+    click.echo("tsf = " + test_suite_file)
+    if host is not None:
+        click.echo("host = " + host)
 
     if ctx.invoked_subcommand is None:
         click.echo(f"Subcommand: {ctx.invoked_subcommand}")
-        ret = ssh_connect_with_config("noble", "~/.ssh/config-container")
+        #ret = ssh_connect_with_config("noble", "~/.ssh/config-container")
+        ret = -1
         click.echo("ssh = " + str(ret))
     else:
         click.echo(f"Sigue: {ctx.invoked_subcommand}")
-        run_test()
 
 
 @persea.command()
@@ -37,3 +46,4 @@ def run():
     """Execute a test"""
 
     click.echo("Run test")
+    #run_test()
